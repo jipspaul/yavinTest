a314d110180769277f5aa5c1b01d22c82cf04047
jp <jacquot.jpaul@gmail.com>
2021-11-21 01:03:56 +0100

    work on home design

--- app/src/main/java/fr/jnvui/yavintest/ui/adapters/TicketAdapter.kt
+++ app/src/main/java/fr/jnvui/yavintest/ui/adapters/TicketAdapter.kt
@@ -20,7 +20,6 @@ class TicketAdapter(
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val ticketType: TextView
         val ticketPrice: TextView
-        val ticketCartCounter: TextView
         val deleteButton: ImageView
         val ticketPriceItem: ConstraintLayout
 
@@ -28,7 +27,6 @@ class TicketAdapter(
             ticketType = view.findViewById(R.id.ticketTypeTextView)
             ticketPrice = view.findViewById(R.id.dayTicketPriceTextView)
             ticketPriceItem = view.findViewById(R.id.ticketPriceItem)
-            ticketCartCounter = view.findViewById(R.id.ticketCounter)
             deleteButton = view.findViewById(R.id.removeItemButton)
         }
     }
@@ -43,27 +41,23 @@ class TicketAdapter(
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
 
         viewHolder.ticketType.text = dataSet[position].ticketType
-        (dataSet[position].ticketPrice + "€").also { viewHolder.ticketPrice.text = it }
+
+        if (dataSet[position].ticketCartCounter > 0) {
+            viewHolder.deleteButton.visibility = View.VISIBLE
+            (dataSet[position].ticketPrice + "€ x" + dataSet[position].ticketCartCounter).also {
+                viewHolder.ticketPrice.text = it
+            }
+        } else {
+            viewHolder.deleteButton.visibility = View.INVISIBLE
+            (dataSet[position].ticketPrice + "€").also { viewHolder.ticketPrice.text = it }
+        }
+
         viewHolder.ticketPriceItem.setOnClickListener {
             onClickListener.onClick(dataSet[position])
         }
 
         viewHolder.deleteButton.setOnClickListener {
             onClickListener.onClickCounterDelete(dataSet[position])
-        }
-
-        //show counter only when its different > 0
-        if (dataSet[position].ticketCartCounter > 0) {
-
-            viewHolder.deleteButton.visibility = View.VISIBLE
-            viewHolder.ticketCartCounter.visibility = View.VISIBLE
-            viewHolder.ticketCartCounter.text = dataSet[position].ticketCartCounter.toString()
-
-        } else {
-
-            viewHolder.deleteButton.visibility = View.INVISIBLE
-            viewHolder.ticketCartCounter.visibility = View.INVISIBLE
-
         }
 
     }
