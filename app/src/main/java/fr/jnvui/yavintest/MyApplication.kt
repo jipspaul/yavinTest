b65baaf56596c78a7538c0053e0965d2df1973e3
jp <jacquot.jpaul@gmail.com>
2021-11-23 10:53:20 +0100

    wip

--- app/src/main/java/fr/jnvui/yavintest/MyApplication.kt
+++ app/src/main/java/fr/jnvui/yavintest/MyApplication.kt
@@ -28,9 +28,9 @@ class MyApplication : Application() {
         val dao: TicketsDAO by inject()
         //TODO create db file
         doAsync {
-            dao.insertAll(Ticket("1", "Single Journey ticket", "1.60", 1))
-            dao.insertAll(Ticket("2", "One day ticket", "12.40", 0))
-            dao.insertAll(Ticket("3", "One week ticket", "47.90", 0))
+            dao.insertAll(Ticket("1", "Single Journey ticket", "1.10", 1))
+            dao.insertAll(Ticket("2", "One day ticket", "2.50", 0))
+            dao.insertAll(Ticket("3", "One week ticket", "12", 0))
         }
     }
 
